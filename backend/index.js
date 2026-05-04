f478786f3f05503c6cd3585846fadd8166890a88
WatamelonP <marlongalang47@gmail.com>
2026-05-04 16:57:04 +0800

    added user schema

---
+++ backend/index.js
@@ -0,0 +1,41 @@
+// DEPENDENCIES || IMPORTS
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+// const userRoutes = require("./routes/user");
+
+// Configurations 
+
+require('dotenv').config();
+
+const app = express();
+
+app.use(express.json());
+
+const corsOptions = {
+
+	origin: ['http://localhost:8000'], // if gagawa na ng frontend, make sure na naka set sa 8000
+	credentials: true,
+	optionsSuccessStatus: 200
+}
+
+mongoose.connect(process.env.MONGODB_STRING);
+
+mongoose.connection.once('open', () => console.log('Now connected to MongoDB Atlas.'))
+
+
+
+// ROUTES 
+
+// app.use("/users", userRoutes);
+
+
+if(require.main === module) {
+
+	app.listen(process.env.PORT || 3000, () => {
+		console.log(`API is now online on port ${process.env.PORT || 3000}`);
+	})
+}
+
+module.exports = { app, mongoose };
